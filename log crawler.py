cecd618f83cab7f6517e23dabd1482cda215b199
PranavTyagi-3 <89564642+PranavTyagi-3@users.noreply.github.com>
2023-05-29 18:27:44 +0530

    #Day25 Leetcode Log crawler

---
+++ log crawler.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minOperations(self, logs):
+        """
+        :type logs: List[str]
+        :rtype: int
+        """
+        st = ["Main"]
+        for i in logs:
+            if i[0].isalnum():
+                st.append(i)
+            elif i=='./':
+                continue
+            elif i=='../' and len(st)>1:
+                st.pop()
+
+        return len(st)-1

15070cef454f9fc24cbee78e366db5389f8b216f
PranavTyagi-3 <89564642+PranavTyagi-3@users.noreply.github.com>
2023-05-31 21:31:37 +0530

    #Day22 Leetcode Fibonnaci Number

---
+++ Fibonnaci Number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        li=[0,1]
+        for i in range(1,n):
+            li.append(li[-1]+li[-2])
+        print(li)
+        return li[n]

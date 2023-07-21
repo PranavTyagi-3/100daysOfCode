d0f9dafac7bb67b333c7479d37b7e7d792034042
PranavTyagi-3 <pranavww@gmail.com>
2023-07-21 11:50:43 +0530

    #Day74 Leetcode Reverse only letter

---
+++ Reverse only letter.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def reverseOnlyLetters(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        i=0
+        j=len(s)-1
+        li=list(s)
+        while i<j:
+            if li[i].isalpha() and li[j].isalpha():
+                li[i],li[j]=li[j],li[i]
+                i+=1
+                j-=1
+            if li[i].isalpha()==False:
+                i+=1
+            if li[j].isalpha()==False:
+                j-=1
+        return ''.join(li)
